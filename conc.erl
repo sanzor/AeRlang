6f0006d1013dc503b5d6ae8cfa9ff804045d3b1e
sanzor <adriansan_93@yahoo.com>
2019-08-14 00:05:20 +0300

    added state

--- conc.erl
+++ conc.erl
@@ -1,12 +1,30 @@
 -module(conc).
 -compile([debug_info]).
--export([pfunc/0]).
+-export([pfunc/0,state/1]).
 
 pfunc()->
     receive
         {From,stop} -> From ! "You have send me aa so we are bb";
         {From,X} -> 
             From ! "Thanks for"++X,
-            pfunc().
+            pfunc()
     end.
+
+state(List)->
+    receive
+        {FROM,cut}->FROM ! {self(),terminated};
+        {FROM,{add,X}}->
+            FROM ! {self(),stored},
+            state([X|List]);
+        {FROM,{del,X}}->
+            case lists:member(X,List) of
+                true ->
+                    FROM ! {self(),deleted},
+                    state(lists:delete(X,List));
+                false->
+                    FROM ! {self(),not_found},
+                    state(List)
+end
+    end.
+        
 

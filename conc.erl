fc1ed9ae36be071b48e034f96c8462c3190e279a
sanzor <adriansan_93@yahoo.com>
2019-08-17 00:06:37 +0300

    added state

--- conc.erl
+++ conc.erl
@@ -4,28 +4,28 @@
 
 pfunc()->
     receive
-        {From,stop} -> From ! "You have send me aa so we are bb";
-        {From,X} -> 
-            From ! "Thanks for"++X,
+        {FROM,stop} -> FROM ! "You have send me aa so we are bb";
+        {FROM,X} -> 
+            FROM ! "Thanks for"++X,
             pfunc()
     end.
 
 state(List)->
-    receive
-        {FROM,done}->ok;
-        {FROM,{store,Elem}}->
-            FROM ! {self(),stored},
-            state([Elem|List]);
-        {FROM,{take,Elem}}->
-            case lists:member(Elem,List) of
-                true -> 
-                    FROM ! {self()!,{ok,Elem}},
-                    state(lists:delete(Elem,List));
+   receive 
+       {FROM,terminate}->FROM !{self(),terminated};
+       {FROM,{take,Elem}}->
+           case lists:member(Elem,List) of
+               true->
+                   FROM!{taken,Elem},
+                   state(lists:delete(Elem,List));
                 false->
-                    FROM !{self()!,not_found},
+                    FROM!{self(),not_found},
                     state(List)
-            end
-    end.
+           end;
+        {FROM,{store,Elem}}->
+                FROM!{self(),added},
+                state([Elem|List])
+   end.
         
                     
 

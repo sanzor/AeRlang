863e75f80a1ecf5d38556ff2700fe37478456cf5
sanzor <adriansan_93@yahoo.com>
2019-09-02 00:26:46 +0300

    added modules for moitoring

--- mon.erl
+++ mon.erl
@@ -1,17 +1,20 @@
 -module(mon).
 -compile_flags([debug_info]).
+-export([worker/1,start/1]).
 
-
+% ctrl+g
 worker(Queue)->
     receive
         die->exit(died);
-        done->exit(Queue,normal);
-        MSG->worker([{time(),MSG}|Queue)
+        gather->exit({Queue,normal});
+        MSG->worker([{time(),MSG}|Queue])
 end.
 start(Shell)->
-    {PID,Monitor}->spawn_monitor(?MODULE,worker,[Shell]),
+    {PID,Monitor}=spawn_monitor(?MODULE,worker,[Shell]),
     receive
-        {"Exit",_}->spawn_monitor(worker)
+        {"EXIT",_,Data}->
+            Shell ! {"Restart worker, data so far:",element(2,Data)},
+            spawn_monitor(worker);
          WMSG->Shell ! WMSG
     end.
 

5a69963ce7a120bff36eb50381b0223bb5d75d16
sanzor <adriansan_93@yahoo.com>
2019-09-04 23:49:47 +0300

    finish worker

--- mon.erl
+++ mon.erl
@@ -3,19 +3,28 @@
 -export([worker/1,start/1]).
 
 % ctrl+g
-worker(Queue)->
-    receive
-        die->exit(died);
-        gather->exit({Queue,normal});
-        MSG->worker([{time(),MSG}|Queue])
+init()->
+    Pid=spawn(?MODULE,restarter),
+    register(Pid,res),
+    Pid
 end.
-start(Shell)->
-    {PID,Monitor}=spawn_monitor(?MODULE,worker,[Shell]),
-    receive
-        {"EXIT",_,Data}->
-            Shell ! {"Restart worker, data so far:",element(2,Data)},
-            spawn_monitor(worker);
-         WMSG->Shell ! WMSG
-    end.
 
+
+restarter(Shell,Queue)->
+    Wk=spawn_link(?MODULE,worker,[]),
+    register(Wk,worker),
+    receive
+        {'EXIT',Pid,}
+        {'EXIT',Pid,{_,peace}}->Shell ! "From res:died peacefully, wont restart";
+        {'EXIT',Pid,{Queue,horrible}}->
+            Shell ! "will restart and send current list",
+            restarter(Shell,Queue)
+        {MSG}->Shell ! {"Unknown msg",MSG}
+end.
+
+
+worker(Queue)->
+    receive 
+        die->exit()
+end.
 
